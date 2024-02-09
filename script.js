1a5f6a00d84ce564f0abac06ed5fab9129cba7c1
Bruno Benicio <carecsnay1@gmail.com>
2024-02-09 15:37:36 -0300

    Feat: Activating modal.

--- script.js
+++ script.js
@@ -6,9 +6,17 @@ pizzaJson.map((item, index) => {
     let pizzaItem = DQS('.models .pizza-item').cloneNode(true);
 
     pizzaItem.querySelector('.pizza-item--img img').src = item.img;
-    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2).replace('.',',')}`;
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2).replace('.', ',')}`;
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
-
+    pizzaItem.querySelector('a').addEventListener('click', (e) => {
+        e.preventDefault();
+        DQS('.pizzaWindowArea').style.opacity = 0;
+        DQS('.pizzaWindowArea').style.display = 'flex';
+        setTimeout(()=>{
+            DQS('.pizzaWindowArea').style.opacity = 1;
+        }, 200);
+        
+    });
     DQS('.pizza-area').append(pizzaItem);
-})
+});
