5d16a92bd700c1db888f2a6d11271274784ecfd6
Bruno Nitro 5 <carecsnay1@gmail.com>
2024-06-23 13:58:04 -0300

    Feat: event list buttons (close modal)

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ pizzaJson.map((item, index) => {
 
     pizzaItem.setAttribute('data-key', index);
     pizzaItem.querySelector('.pizza-item--img img').src = item.img;
-    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2).replace('.', ',')}`;
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toLocaleString('pt-BR', { minimumFractionDigits: 2 })}`;
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
     pizzaItem.querySelector('a').addEventListener('click', (e) => {
@@ -20,11 +20,11 @@ pizzaJson.map((item, index) => {
         DQS('.pizzaBig img').src = pizzaJson[key].img;
         DQS('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
         DQS('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
-        DQS('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[key].price.toFixed(2).replace('.',',')}`;
+        DQS('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[key].price.toLocaleString('pt-BR', { minimumFractionDigits: 2 })}`;
 
         DQS('.pizzaInfo--size.selected').classList.remove('selected');
-        DQSA('.pizzaInfo--size').forEach((size, sizeIndex)=>{
-            if(sizeIndex == 2) {
+        DQSA('.pizzaInfo--size').forEach((size, sizeIndex) => {
+            if (sizeIndex == 2) {
                 size.classList.add('selected');
             }
             size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex];
@@ -32,9 +32,19 @@ pizzaJson.map((item, index) => {
         DQS('.pizzaInfo--qt').innerHTML = modalQT;
         DQS('.pizzaWindowArea').style.opacity = 0;
         DQS('.pizzaWindowArea').style.display = 'flex';
-        setTimeout(()=>{
+        setTimeout(() => {
             DQS('.pizzaWindowArea').style.opacity = 1;
         }, 200);
     });
     DQS('.pizza-area').append(pizzaItem);
 });
+
+// Events modal
+const closeModal = () => {
+    DQS('.pizzaWindowArea').style.opacity = 0;
+    setTimeout(() => {
+        DQS('.pizzaWindowArea').style.display = 'none';
+    }, 500);
+}
+
+DQSA('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach(item => item.addEventListener('click', closeModal));
