2f349a07ccf997cf9eaed15583d05355aafc5efc
Bruno Nitro 5 <carecsnay1@gmail.com>
2024-06-30 20:17:26 -0300

    Feat: increase or decrease the modal's quantity.

--- script.js
+++ script.js
@@ -5,46 +5,66 @@ const DQS = (el) => document.querySelector(el);
 const DQSA = (el) => document.querySelectorAll(el);
 
 pizzaJson.map((item, index) => {
-    let pizzaItem = DQS('.models .pizza-item').cloneNode(true);
+  let pizzaItem = DQS(".models .pizza-item").cloneNode(true);
 
-    pizzaItem.setAttribute('data-key', index);
-    pizzaItem.querySelector('.pizza-item--img img').src = item.img;
-    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toLocaleString('pt-BR', { minimumFractionDigits: 2 })}`;
-    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
-    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
-    pizzaItem.querySelector('a').addEventListener('click', (e) => {
-        let key = e.target.closest('.pizza-item').getAttribute('data-key');
-        e.preventDefault();
-        modalQT = 1;
+  pizzaItem.setAttribute("data-key", index);
+  pizzaItem.querySelector(".pizza-item--img img").src = item.img;
+  pizzaItem.querySelector(
+    ".pizza-item--price"
+  ).innerHTML = `R$ ${item.price.toLocaleString("pt-BR", {
+    minimumFractionDigits: 2,
+  })}`;
+  pizzaItem.querySelector(".pizza-item--name").innerHTML = item.name;
+  pizzaItem.querySelector(".pizza-item--desc").innerHTML = item.description;
+  pizzaItem.querySelector("a").addEventListener("click", (e) => {
+    let key = e.target.closest(".pizza-item").getAttribute("data-key");
+    e.preventDefault();
+    modalQT = 1;
 
-        DQS('.pizzaBig img').src = pizzaJson[key].img;
-        DQS('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
-        DQS('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
-        DQS('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[key].price.toLocaleString('pt-BR', { minimumFractionDigits: 2 })}`;
+    DQS(".pizzaBig img").src = pizzaJson[key].img;
+    DQS(".pizzaInfo h1").innerHTML = pizzaJson[key].name;
+    DQS(".pizzaInfo--desc").innerHTML = pizzaJson[key].description;
+    DQS(".pizzaInfo--actualPrice").innerHTML = `R$ ${pizzaJson[
+      key
+    ].price.toLocaleString("pt-BR", { minimumFractionDigits: 2 })}`;
 
-        DQS('.pizzaInfo--size.selected').classList.remove('selected');
-        DQSA('.pizzaInfo--size').forEach((size, sizeIndex) => {
-            if (sizeIndex == 2) {
-                size.classList.add('selected');
-            }
-            size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex];
-        });
-        DQS('.pizzaInfo--qt').innerHTML = modalQT;
-        DQS('.pizzaWindowArea').style.opacity = 0;
-        DQS('.pizzaWindowArea').style.display = 'flex';
-        setTimeout(() => {
-            DQS('.pizzaWindowArea').style.opacity = 1;
-        }, 200);
+    DQS(".pizzaInfo--size.selected").classList.remove("selected");
+    DQSA(".pizzaInfo--size").forEach((size, sizeIndex) => {
+      if (sizeIndex == 2) {
+        size.classList.add("selected");
+      }
+      size.querySelector("span").innerHTML = pizzaJson[key].sizes[sizeIndex];
     });
-    DQS('.pizza-area').append(pizzaItem);
+    DQS(".pizzaInfo--qt").innerHTML = modalQT;
+    DQS(".pizzaWindowArea").style.opacity = 0;
+    DQS(".pizzaWindowArea").style.display = "flex";
+    setTimeout(() => {
+      DQS(".pizzaWindowArea").style.opacity = 1;
+    }, 200);
+  });
+  DQS(".pizza-area").append(pizzaItem);
 });
 
 // Events modal
 const closeModal = () => {
-    DQS('.pizzaWindowArea').style.opacity = 0;
-    setTimeout(() => {
-        DQS('.pizzaWindowArea').style.display = 'none';
-    }, 500);
-}
+  DQS(".pizzaWindowArea").style.opacity = 0;
+  setTimeout(() => {
+    DQS(".pizzaWindowArea").style.display = "none";
+  }, 500);
+};
 
-DQSA('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach(item => item.addEventListener('click', closeModal));
+DQSA(".pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton").forEach(
+  (item) => item.addEventListener("click", closeModal)
+);
+
+DQS(".pizzaInfo--qtmenos").addEventListener("click", () => {
+  if (modalQT > 1) {
+    modalQT--;
+    DQS(".pizzaInfo--qt").innerHTML = modalQT;
+  }
+});
+
+DQS(".pizzaInfo--qtmais").addEventListener("click", () => {
+  modalQT++;
+  DQS(".pizzaInfo--qt").innerHTML = modalQT;
+});
